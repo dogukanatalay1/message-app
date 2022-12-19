86ebdb81befa6fcf0111f4ac72026f7654eaac82
DOGUKAN ATALAY <dogukanatalay46@gmail.com>
2022-12-19 15:08:48 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app"><Message /></div>
+</template>
+
+<script>
+import Message from "../src/components/message/Message.vue";
+
+export default {
+  name: "App",
+  components: {
+    Message,
+  },
+};
+</script>
+
+<style></style>
